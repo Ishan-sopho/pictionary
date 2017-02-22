7871fc0a7bcca1379ec42d17bf1b156a3756ac75
Ishan-sopho <bhanuka.ishan@gmail.com>
2017-02-22 20:59:25 +0530

    display and data transfer
    tkinter display for user and team
    socket connection to transfer data

---
+++ tkinter_team_display.py
@@ -0,0 +1,7 @@
+from Tkinter import *
+
+message1='write your answers here'
+root = Tk()
+text = Text(root, relief=SUNKEN, width=25, height=4).pack(fill=message1)
+mainloop()
+
--- word extractor.py
+++ word extractor.py
@@ -2,29 +2,19 @@ import openpyxl
 wb = openpyxl.load_workbook('Pictionary-Words.xlsx')
 sheet = wb.get_sheet_by_name('Table 1')
 noun,easy,medium,hard=[],[],[],[]
+feasy=open(r'easynouns.txt','w')
+fmedium=open(r'mediumnouns.txt','w')
+fhard=open(r'hardnouns.txt','w')
 for i in range (1,4,1):
     for j in range (3,23,1):
-        easy.append(sheet.cell(row=j,column=i).value)
-    noun.append(easy)
+        feasy.write((sheet.cell(row=j,column=i).value).encode('utf-8'))
+        feasy.write('\n')
     for k in range (24,67,1):
-        medium.append(sheet.cell(row=k,column=i).value)
-    noun.append(medium)
+        fmedium.write((sheet.cell(row=k, column=i).value).encode('utf-8'))
+        fmedium.write('\n')
     for l in range (68,111,1):
-        hard.append(sheet.cell(row=l,column=i).value)
-    noun.append(hard)
-for a in noun:
-    for b in a:
-        print b
-import random
-word=0
-while (raw_input(':')!='stop'):
-    n=random.randint(1,10)
-    if n<=3:
-        word=random.randint(0,59)
-        print noun[0][word]
-    elif n<=7:
-        word=random.randint(0,128)
-        print noun[1][word]
-    else:
-        word=random.randint(0,128)
-        print noun[2][word]
+        fhard.write((sheet.cell(row=l, column=i).value).encode('utf-8'))
+        fhard.write('\n')
+feasy.close()
+fmedium.close()
+fhard.close()
